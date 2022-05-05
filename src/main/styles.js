222f09a64215955759044b92000694a8dd0a2377
Atharvashuks <atharvashuklaas@gmail.com>
2022-05-05 19:21:59 +0530

    initial commit

---
+++ src/main/styles.js
@@ -0,0 +1,12 @@
+export const color = {
+  button: '#EF0107',
+  gery1: '#43484d',
+  grey2: '#5e6977',
+  grey3: '#86939e',
+  grey4: '#bdc6cf',
+  bg: '#fff',
+};
+
+export const parameter = {
+  headerHeight: 50,
+};
